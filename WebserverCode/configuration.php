3960223f1f9db3deaf87fa69daa5acee749d35fc
Shota Ehrlich <shotaehrlich@yahoo.com>
2018-04-29 15:36:59 -0500

    Update configuration.php

--- WebserverCode/configuration.php
+++ WebserverCode/configuration.php
@@ -5,31 +5,31 @@
 
 	// Enter Host URL for databse here
 	function getHostUrl(){
-		$Host_URL = 'database.cse.tamu.edu';
+		$Host_URL = '<database url>';
 		return $Host_URL;
 	}
 
 	// Enter username for databse here
 	function getUsername(){
-		$UserName = 'shotaehrlich';
+		$UserName = '<username>';
 		return $UserName;
 	}
 
 	// Enter password for databse here
 	function getPassword(){
-		$Password = 'mmDS844825';
+		$Password = '<password>';
 		return $Password;
 	}
 
 	// Enter name for databse here
 	function getDatabaseName(){
-		$Database_Name = 'shotaehrlich';
+		$Database_Name = '<database>';
 		return $Database_Name;
 	}
 
 	// Enter name for table here
 	function getTableName(){
-		$Table_Name = 'test';
+		$Table_Name = '<tablename>';
 		return $Table_Name;
 	}
 ?>
